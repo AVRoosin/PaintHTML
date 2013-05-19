4751f519ad1dc705dd699c54a2348de659acd1aa
AVRoosin <avroosin@mail.ru>
2013-05-19 20:12:33 +0600

    Initial commit

---
+++ PaintHTML/js/KnockoutBackgroundCode.js
@@ -0,0 +1,15 @@
+﻿$(function() {
+    var viewModel={
+        name: ko.observable("TolikR"),
+        changeName: function(){
+            this.name("AVRoosin");
+        },
+        nameVisible: ko.observable(true)
+    }
+    //viewModel.displayName = ko.dependentObservable(function(){
+    //    return this.name = " is" + (!this.nameVisible() ? " not":"")+ " visible";
+    //},viewModel);
+    //fillColorVisible: ko.observable(true);
+    ko.applyBindings(viewModel);
+}
+ );
